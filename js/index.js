4aa80d1f9d2508bae771670a2ef3a27c43ea6231
Leonelsk8 <leonelgomez.sk8@gmail>
2023-02-14 16:58:06 -0300

    actualizando

--- js/index.js
+++ js/index.js
@@ -7,7 +7,9 @@ document.getElementById('gameDes').appendChild(destacado);
 const info = document.createElement('div');
 info.className = 'text-white divDes animate__animated animate__fadeInLeft col-12 col-md-8 col-lg-6';
 info.innerHTML = 
-` <h3 class="mb-1 mb-md-2 mb-lg-3 title">${game.title}</h3>
+` <div class="mb-1 mb-md-2 mb-lg-3 title">${game.title} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill text-warning d-none d-md-inline" viewBox="0 0 16 16">
+<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg></div>
   <p class="description">${game.description.slice(0, 300) + "..."}</p>
   <div class="d-flex justify-content-center justify-content-md-start">
     <button class="btn botonDes" onclick="vermasDes()"><i class="fa-solid fa-circle-info"></i>Ver más</button>
