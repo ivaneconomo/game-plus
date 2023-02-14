b57e97e72b78f7087d208ab5d0f08fe06af6f1a8
ivaneconomo <ivan.economo39@gmail.com>
2023-02-14 13:31:48 -0300

    update pagina registro

--- js/register.js
+++ js/register.js
@@ -1,21 +1,23 @@
 const users = JSON.parse(localStorage.getItem('Users'));
-const validarEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+const regexEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 const crearUsuario = () => {
-    const nombreRegistro = document.getElementById('nombreRegistro').value;
-    const emailIngresado = document.getElementById('emailRegistro').value.toLowerCase();
-    let emailRegistro;
-    const contrasenaIngresada = document.getElementById('contrasenaRegistro').value;
-    const repContrasenaIngresada = document.getElementById('repContrasenaRegistro').value;
-    let contrasenaRegistro;
-    // Validando email
+  const nombreRegistro = document.getElementById('nombreRegistro').value;
+  const emailIngresado = document.getElementById('emailRegistro').value.toLowerCase();
+  let emailRegistro = '';
+  const contrasenaIngresada = document.getElementById('contrasenaRegistro').value;
+  const repContrasenaIngresada = document.getElementById('repContrasenaRegistro').value;
+  let contrasenaRegistro = '';
+
+  // Validando email.
+  const validarEmail = () => {
     try {
-        if (validarEmail.test(emailIngresado)) {
+        if (regexEmail.test(emailIngresado)) {
             users.map((user) => {
                 if (user.email !== emailIngresado) {
                     emailRegistro = emailIngresado;
                     return emailRegistro;
                 } else {
-                    throw new Error('El email ingresado ya se encuentra registrado.');
+                  throw new Error('El email ingresado ya se encuentra registrado.');
                 };
             });
         } else {
@@ -24,30 +26,43 @@ const crearUsuario = () => {
     } catch (error) {
         alert(error.message);
     };
+  };
+  validarEmail();
 
-    // Validando contraseñas
-    const validarContraseña = () => {
-        try {
-            if (contrasenaIngresada === repContrasenaIngresada) {
-                contrasenaRegistro = contrasenaIngresada;
-                return contrasenaRegistro;
-            } else {
-                throw new Error('Las contraseñas ingresadas no coinciden.')
-            };
-        } catch (error) {
-            alert(error.message);
+  // Validando contraseñas.
+  const validarContraseña = () => {
+      try {
+          if (contrasenaIngresada === repContrasenaIngresada) {
+              contrasenaRegistro = contrasenaIngresada;
+              return contrasenaRegistro;
+          } else {
+              throw new Error('Las contraseñas ingresadas no coinciden.')
+          };
+      } catch (error) {
+          alert(error.message);
+      };
+  };
+  validarContraseña();
+
+  // Registrando usuario con los datos validados.
+  console.log({nombreRegistro, emailRegistro, contrasenaRegistro});
+  const registrarUsuario = () => {
+    try {
+      if (!(nombreRegistro == ''|| emailRegistro == ''|| contrasenaRegistro == '')) {
+        const nuevoUsuario = {
+          name: nombreRegistro,
+          id: users.length +1,
+          email: emailRegistro,
+          password: contrasenaRegistro,
         };
+        users.push(nuevoUsuario);
+        localStorage.setItem('Users', JSON.stringify(users));
+      } else {
+        throw new Error('Debe completar todos los campos de manera correcta.');
+      };
+    } catch (error) {
+      alert(error.message);
     };
-    validarContraseña();
-
-    console.log({nombreRegistro, emailRegistro, contrasenaRegistro});
-
-
-
-//   const nuevoUsuario = {
-//     name: document.getElementById('nombreRegsitro').value,
-//     id: users.lenght ++,
-//     email: document.getElementById('emailRegistro').value,
-//     password: document.getElementById('contrasenaRegistro').value,
-//   }
-}; 
+  };
+  registrarUsuario();
+};
