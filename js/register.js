e182aa7341c611ab378322ca62c34a09c9b15766
ivaneconomo <ivan.economo39@gmail.com>
2023-02-17 19:18:39 -0300

    update pagina registro

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 const users = JSON.parse(localStorage.getItem('Users'));
 
 let nombreRegistro = '';
-let emailRegistro = 'admin123@gmail.com';
+let emailRegistro = '';
 let contrasenaRegistro = '';
 let contrasenaIngresada = '';
 let repContrasenaIngresada = '';
@@ -27,17 +27,18 @@ const validarEmail = () => {
 
   try {
       if (regexEmail.test(emailIngresado)) {
-          users.map((user) => {
-              if (emailIngresado !== user.email && emailIngresado !== '') {
-                  emailRegistro = emailIngresado;
-                  document.getElementById('emailDisp').classList.remove('d-none');
-                  return emailRegistro;
-              } else {
-                document.getElementById('emailNoDisp').classList.remove('d-none');
-                emailRegistro = '';
-                throw new Error('Email ingresado ya registrado.');
-              };
+        const findEmail = users.find((user) => {
+            return user.email == emailIngresado;
           });
+        if (findEmail == undefined) {
+          emailRegistro = emailIngresado;
+          document.getElementById('emailDisp').classList.remove('d-none');
+          return emailRegistro;
+        } else {
+          document.getElementById('emailNoDisp').classList.remove('d-none');
+          emailRegistro = '';
+          throw new Error('Email ingresado ya registrado.');
+        };  
       } else {
         document.getElementById('formatoInvalido').classList.remove('d-none');
         emailRegistro = '';
@@ -117,17 +118,53 @@ document.getElementById('repContrasenaRegistro').addEventListener('change', veri
 
 
 
-const found = users.find( (user) => {
-  try {
-    if (emailRegistro !== user.email && emailRegistro !== '') {
-      return 
-    } else {
-      
-    }
-  } catch (error) {
-    
-  }
-})
+////////////////////////////////////////////////////////////////////////////
+// Devuelve el correo encontrado o de lo contrario devuelve Undefined
+// const findEmail = users.find((user) => {
+//   console.log(user.email);
+//   return user.email == emailIngresado;
+// });
+
+// if (findEmail == undefined) {
+//   console.log(`Email disponible: ${emailIngresado}`);
+// } else {
+//   console.log(`Email NO disponible: ${emailIngresado}`);
+// };
 
 
+// const validarEmail = () => {
+//   const regexEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+//   const emailIngresado = document.getElementById('emailRegistro').value.toLowerCase();
+  
+//   if (!document.getElementById('emailDisp').classList.contains('d-none')) {
+//     document.getElementById('emailDisp').classList.add('d-none');
+//   };
+//   if (!document.getElementById('emailNoDisp').classList.contains('d-none')) {
+//     document.getElementById('emailNoDisp').classList.add('d-none');
+//   };
+//   if (!document.getElementById('formatoInvalido').classList.contains('d-none')) {
+//     document.getElementById('formatoInvalido').classList.add('d-none');
+//   };
 
+//   try {
+//       if (regexEmail.test(emailIngresado)) {
+//           users.map((user) => {
+//               if (emailIngresado !== user.email && emailIngresado !== '') {
+//                   emailRegistro = emailIngresado;
+//                   document.getElementById('emailDisp').classList.remove('d-none');
+//                   return emailRegistro;
+//               } else {
+//                 document.getElementById('emailNoDisp').classList.remove('d-none');
+//                 emailRegistro = '';
+//                 throw new Error('Email ingresado ya registrado.');
+//               };
+//           });
+//       } else {
+//         document.getElementById('formatoInvalido').classList.remove('d-none');
+//         emailRegistro = '';
+//         throw new Error('Formato email no válido.');
+//       };
+//   } catch (error) {
+//     console.warn(error.message);
+//   };
+// };
